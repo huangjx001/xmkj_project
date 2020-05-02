c84a6edc863f05d6a441e1a0dc7c5f3767cf2194
huangjx <694476767@qq.com>
2020-05-02 20:35:39 +0800

    基礎版本模塊，微服务

--- xmkj_uua/src/main/java/com/zz/xmkj/XmkjUaaApplication.java
+++ xmkj_uua/src/main/java/com/zz/xmkj/XmkjUaaApplication.java
@@ -3,8 +3,10 @@ package com.zz.xmkj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class XmkjUaaApplication
 {
