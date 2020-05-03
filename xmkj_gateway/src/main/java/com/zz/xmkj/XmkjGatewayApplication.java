4aaccfc67ea9f4edb41e22ebe648452f5bd1655a
huangjx <694476767@qq.com>
2020-05-03 16:47:47 +0800

    网关

--- xmkj_gateway/src/main/java/com/zz/xmkj/XmkjGatewayApplication.java
+++ xmkj_gateway/src/main/java/com/zz/xmkj/XmkjGatewayApplication.java
@@ -1,13 +1,19 @@
 package com.zz.xmkj;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
-public class XmkjGatewayApplication {
+@EnableDiscoveryClient
+public class XmkjGatewayApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(XmkjGatewayApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(XmkjGatewayApplication.class, args);
+    }
 
 }
